6aeaeef360a03d088cbbd09cacb19e5a42759a90
Tdead1 <32958884+Tdead1@users.noreply.github.com>
2023-07-31 15:28:18 +0200

    Cleaning up hello rust

--- src/main.rs
+++ src/main.rs
@@ -11,27 +11,17 @@ use rodio::{Decoder, OutputStream, source::Source};
 fn main() {
     let mut window = Window::new("Kiss3d: cube");
     let mut cube = window.add_cube(0.5, 0.5, 0.5);
-
+	
     cube.set_color(1.0, 0.0, 0.0);
     window.set_light(Light::StickToCamera);
 
     let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
-
-	// Get a output stream handle to the default physical sound device
-	// Load a sound from a file, using a path relative to Cargo.toml
-	// Decode that sound file into a source
-	// Play the sound directly on the device
 	let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 	let file = BufReader::new(File::open("data/guitar.mp3").unwrap());
-	let source = Decoder::new(file).unwrap();
-	let err: Result<(), rodio::PlayError> = stream_handle.play_raw(source.convert_samples());
-
-	if !err.is_ok()
-	{
-   		cube.set_color(0.0, 1.0, 0.0);
-	}
-
-    while window.render() 
+	let decoder = Decoder::new(file).unwrap();
+	let audio_result: Result<(), rodio::PlayError> = stream_handle.play_raw(decoder.convert_samples());
+    
+	while window.render() && audio_result.is_ok()
 	{
         cube.prepend_to_local_rotation(&rot);
     }
